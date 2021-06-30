13e0fc2c5ada62f9718249468c07ac4ee8d257cf
Tatjana14 <tanycha1403@gmail.com>
2021-06-30 22:39:43 +0300

    change components

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,11 +1,24 @@
 import s from './Dialogs.module.scss'
 
 
-
-const Dialogs = () =>{
-    return(
-        <div className={s.container}>
-            Dialogs
+const Dialogs = () => {
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogs_items}>
+                <div className={s.dialog_item}>
+                    <span>Ilja</span>
+                </div>
+                <div className={s.dialog_item}>
+                    <span>Inna</span>
+                </div>
+                <div className={s.dialog_item}>
+                    <span>Polina</span>
+                </div>
+                <div className={s.dialog_item}>
+                    <span>Lena</span>
+                </div>
+            </div>
+            <div className="message"></div>
         </div>
     )
 }
