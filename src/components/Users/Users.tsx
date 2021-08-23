88b312bb772728007b34fb4fe1799241ca8ff81a
Tatjana14 <tanycha1403@gmail.com>
2021-08-23 11:58:47 +0300

    add componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,8 +5,7 @@ import axios from "axios";
 import user from '../../assets/img/user.png'
 
 class Users extends React.Component<UsersPropsType, any> {
-    constructor(props: UsersPropsType) {
-        super(props);
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
             this.props.setUsers(response.data.items)
         })
