2652b149882b5b7fd2f2ef53fc83349ae52e9322
Tatjana14 <tanycha1403@gmail.com>
2021-09-22 23:05:13 +0300

    setState

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,23 +1,23 @@
 import s from './ProfileInfo.module.scss'
 import {UserProfileType} from "../../../redux/profile-reducer";
 import Preloader from "../../Preloader/Preloader";
+import ProfileStatus from "../ProfileStatus/ProfileStatus";
 type ProfileInfoPropsType = {
     profile: UserProfileType | undefined
 }
 const ProfileInfo: React.FC<ProfileInfoPropsType> = (props) => {
-    if(!props.profile){
+    /*if(!props.profile){
         return <Preloader/>
-    } else{
+    } else{}*/
         return (
             <div className={s.container}>
                 <div className={s.profile_bg}></div>
                 <div>
-                    <img src={props.profile.photos.large} alt=""/>
+                    {props.profile? <img src={props.profile.photos.large} alt=""/> : ''}
+                    <ProfileStatus status={'heyhey'}/>
                     ava+decription</div>
             </div>
         );
-    }
-
 }
 
 export default ProfileInfo;
